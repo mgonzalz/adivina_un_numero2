dfdce7c788d0fb095d0216c848ac5c1dd4e5fc26
mgonzalz <mgonzgarc@myuax.com>
2022-11-15 01:48:23 +0100

    c8

--- adivina_numero.py
+++ adivina_numero.py
@@ -23,19 +23,46 @@ def intro():
 def juega(minimo, maximo):
     import random
     numero = random.randint(minimo, maximo)
-    print(numero)
-    ayuda = 0
+    print (numero)
+    ayuda = 0 #HACER PARA QUE SE REPITA LA FUNCION DE VARIABLE Y NO SE ACTUALICE SIEMPRE QUE SE REPITA A CERO
     while True:
-        print("Introduzca un numero entre", minimo, "y", maximo)
+        print ("Introduzca un numero entre", minimo, "y", maximo)
         intento = int(input())
         if intento == numero:
             ayuda+=1
-            print("Has acertado. Se han necesitado", ayuda, "intentos")
+            print ("Has acertado. Se han necesitado", ayuda, "intentos")
+            victoria()
             break
+        elif intento < numero:
+            print ("El numero es mayor")
+            ayuda+=1
         elif intento > numero:
-            print("Demasiado grande")
+            print ("Demasiado grande")
             ayuda+=1
-        else: # intento < numero
-            print("Demasiado pequeño")
+        elif intento < numero:
+            print ("Demasiado pequeño")
             ayuda+=1
+        else:
+            return juega() #ARREGLAR QUE SI SE PONE LETRA U OTRA COSA QUE NO SEA NUMERO PERMITA INTENTARLO OTRA VEZ
+def ayuda(minimo, maximo, ayuda):
+    if ayuda == 5:
+        print (input("¿Necesita ayuda? (S/N): "))
+        if ayuda == "S":
+            print ("El numero está entre", maximo -(maximo/2), "y", minimo -(minimo/2))
+        elif ayuda == "N":
+            juega(minimo, maximo, ayuda)
+    else:
+        juega(minimo, maximo)
+def victoria():
+    NomUsuario = input("Introduzca su nombre: ")
+    print (NomUsuario)
+    print (NomUsuario, input(",¿Quiere que se guarde su puntuación? (S/N):"))
+    if NomUsuario == "S":
+        print ("Su puntuación se ha guardado")
+        lista.append(NomUsuario + " " + ayuda)
+        print (lend(lista))     #ARREGLAR LA LISTA
+    elif NomUsuario == "N":
+        print ("Su puntuación no se ha guardado")
+        intro()
+
 intro()
