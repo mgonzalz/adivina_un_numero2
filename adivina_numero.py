f2d236b6e0453b7fd89f39f090cf0a4aba252371
mgonzalz <mgonzgarc@myuax.com>
2022-11-17 18:26:01 +0100

    c13

--- adivina_numero.py
+++ adivina_numero.py
@@ -9,7 +9,7 @@ def intro():
     nivel = input("Introduzca el nivel de dificultad: ")
     ayuda = 0
     if nivel == "1":
-        juega(0, 100, 0)
+        juega(0, 100, 0, numero_aleatorio(0, 100))
     elif nivel == "2":
         juega(0, 1000, 0)
     elif nivel == "3":
@@ -25,37 +25,40 @@ def numero_aleatorio(minimo, maximo):
     import random
     numero = random.randint(minimo, maximo)
     return numero
-def juega(minimo, maximo, ayuda):
-    numero = numero_aleatorio(minimo, maximo)
-    print (numero)
+def numero_intento(minimo, maximo):
     while True:
-        print ("Introduzca un numero entre", minimo, "y", maximo)
-        intento = input()
+        intento = input("Introduzca un número entre " + str(minimo) + " y " + str(maximo) + ": ")
         try:
             intento = int(intento)
         except:
             pass
         else:
+            # Realizar la comparación
             if minimo <= intento <= maximo:
-                break
+                return intento
+def juega(minimo, maximo, ayuda, numero):
+    print (numero)
+    while True:
+        intento = numero_intento(minimo, maximo)
         if intento == numero:
             ayuda+=1
             print ("Has acertado. Se han necesitado", ayuda, "intentos")
             victoria(ayuda)
             break
         elif intento < numero:
-            print ("El numero es mayor")
+            print ("Demasiado pequeño")
             ayuda+=1
             if ayuda == 5:
                 ayudaf(minimo, maximo, numero, ayuda)
+            elif ayuda == 10:
+                maximointentos(minimo, maximo, ayuda)
         elif intento > numero:
             print ("Demasiado grande")
             ayuda+=1
             if ayuda == 5:
                 ayudaf(minimo, maximo, numero, ayuda)
-        elif intento < numero:
-            print ("Demasiado pequeño")
-            ayuda+=1
+            elif ayuda == 10:
+                maximointentos(minimo, maximo, ayuda)
         else:
             return True
 def ayudaf(minimo, maximo, numero, ayuda):
@@ -68,28 +71,45 @@ def ayudaf(minimo, maximo, numero, ayuda):
     nuevomax = random.randint(numero, MAX)
     if pregunta == "S" or pregunta == "s":
         print ("El numero está se encuentra entre los siguientes valores:",nuevomin, ", ", nuevomax)
-        juega(nuevomin, nuevomax, ayuda)
+        juega(nuevomin, nuevomax, ayuda, numero)
     elif pregunta == "N" or pregunta == "n":
         juega()
     else:
         return ayudaf(minimo, maximo, numero, ayuda)
+def maximointentos(minimo, maximo, ayuda):
+    if ayuda == 10:
+        print ("Has perdido. Alcanzaste el máximo numero de intentos:", ayuda)
+        p = input("¿Quiere volver a jugar? (S/N): ")
+        print (p)
+        if p == "S" or p == "s":
+            intro()
+        elif p == "N" or p == "n":
+            print ("Gracias por jugar")
+            exit()
+        else:
+            return maximointentos(minimo, maximo, ayuda)
 def victoria(ayuda):
     NomUsuario = input("Introduzca su nombre: ")
     print (NomUsuario)
     print (NomUsuario, ",¿Quiere que se guarde su puntuación? (S/N):")
     respuesta = input()
-    if respuesta == "S":
-        Ganador = NomUsuario + " " + str(ayuda)
-        list.append(Ganador)
-        for Ganador in list:
-            print ("Nombre: ", NomUsuario, "Intentos: ", ayuda)
+    if respuesta == "S" or respuesta == "s":
         print ("Su puntuación se ha guardado")
+        listaGanadores = []
+        listaGanadores.append(NomUsuario + " con " + str(ayuda) + " intentos")
+        print (listaGanadores)
+        print ("¿Quiere volver a jugar? (S/N): ")
+        q = input()
+        if q == "S" or q == "s":
+            intro()
+        elif q == "N" or q == "n":
+            print ("Gracias por jugar")
+            exit()
     elif respuesta == "N":
         print ("Su puntuación no se ha guardado")
+        print("Gracias por jugar")
         intro()
     else:
         victoria()
-#DETERMINAR UN N MAXIMO DE INTENTOS
-#MOSTRAR LISTA DE PUNTUACIONES
 #INTELIGENCIA ARTIFICIAL
 intro()
